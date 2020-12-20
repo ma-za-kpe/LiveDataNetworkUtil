2d4097f799a6b360887fba556df2add436631fb6
MAZAKPE <makpalyy@gmail.com>
2020-12-20 18:33:28 +0300

    CReate initial example project and network library module.

---
+++ networkUtil/src/main/java/com/maku/networkutil/util/Test.kt
@@ -0,0 +1,10 @@
+package com.maku.networkutil.util
+
+import android.util.Log
+
+class Test {
+    fun getString(s: String) : String{
+        Log.d("Test class","string is: $s")
+        return s
+    }
+}
