ab83913474af6595f5aa9222771069c0c88c10ba
MAZAKPE <makpalyy@gmail.com>
2020-12-20 19:05:13 +0300

    update the example project

--- app/src/main/java/com/maku/networkutilexample/MainActivity.kt
+++ app/src/main/java/com/maku/networkutilexample/MainActivity.kt
@@ -2,10 +2,25 @@ package com.maku.networkutilexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.maku.networkutil.util.NetworkUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handleNetwork()
     }
+
+    private fun handleNetwork() {
+        NetworkUtil.getNetworkLiveData(applicationContext).observe(this, { isConnected ->
+            if (!isConnected) {
+                Toast.makeText(this, "no internet", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "internet available", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
 }
